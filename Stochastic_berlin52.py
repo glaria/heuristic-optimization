1a89120d9494d481b525a2fe9921db3b2d4336ed
LAGS <glaria21@gmail.com>
2024-12-31 16:21:01 +0100

    optimized functions

--- Stochastic_berlin52.py
+++ Stochastic_berlin52.py
@@ -5,15 +5,9 @@ Created on Sun Jan 16 11:05:42 2022
 @author: Luis Alberto Glaría Silva
 """
 
-#import numpy as np
+import numpy as np
 import math
 
-def euclideanDistance(xNode, yNode):
-    sum = 0.0
-    
-    for xi, yi in zip(xNode, yNode):
-        sum += pow((xi-yi), 2)
-    return math.sqrt(sum)
 
 berlin52 = [[565,575], [25, 185], [345, 750],[945, 685], [845,655],
             [880,660], [25,230], [525,1000],[580, 1175], [650, 1130],[1605,620],
@@ -26,13 +20,38 @@ berlin52 = [[565,575], [25, 185], [345, 750],[945, 685], [845,655],
             [830,610],[605,625], [595,360],[1340,725],[1740,245]]
 
 
-def generateStochasticDataset(dataset):
+def euclidean_distance(x_node, y_node):
+    return math.sqrt(sum((xi - yi) ** 2 for xi, yi in zip(x_node, y_node)))
+
+def generate_stochastic_dataset(dataset, stochastic=False):
     berlin52_stochastic = {}
-    berlin52 = dataset
-    for node in berlin52:
-        berlin52_stochastic[str(node)] = {}
-        for node1 in berlin52:
-             berlin52_stochastic[str(node)][str(node1)] = ([euclideanDistance(node,node1), euclideanDistance(node,node1)])
-         #berlin52_stochastic[str(node)][str(node1)] = (np.random.lognormal(euclideanDistance(node,node1), euclideanDistance(node,node1), 10))
+    n = len(dataset)
+    
+    # Iterate over unique pairs using the upper triangle of the distance matrix
+    for i in range(n):
+        node_a = str(dataset[i])
+        berlin52_stochastic[node_a] = {}
+        
+        for j in range(i, n):
+            node_b = str(dataset[j])
+            
+            # Calculate Euclidean distance
+            distance = euclidean_distance(dataset[i], dataset[j])
+            
+            # Add stochasticity if required
+            if stochastic:
+                distance = np.random.lognormal(mean=distance, sigma=0.1)
+            
+            # Store symmetric distances
+            berlin52_stochastic[node_a][node_b] = distance
+            berlin52_stochastic.setdefault(node_b, {})[node_a] = distance
+
     return berlin52_stochastic
+
+
+# Generate the dataset with optional stochasticity
+stochastic_dataset = generate_stochastic_dataset(berlin52, stochastic=False)
+
+# Example: Print the distance between two specific nodes
+print("Distance between [565, 575] and [25, 185]:", stochastic_dataset["[565, 575]"]["[25, 185]"])
 
